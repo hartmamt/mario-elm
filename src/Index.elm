1fbb4af389d90d9140aeb59fd3607b66771c8404
Matthew Hartman <hartmamt@Matthews-MacBook-Pro.local>
2016-01-10 10:54:27 -0500

    working on cloud platform

--- src/Index.elm
+++ src/Index.elm
@@ -16,6 +16,14 @@ type alias Model =
   , dir : Direction
   }
 
+type alias Platform =
+  { x : Float
+  , y : Float
+  , w: Float
+  , vx : Float
+  , vy : Float
+  }
+
 
 type Direction = Left | Right
 
@@ -32,48 +40,73 @@ mario =
   , dir = Right
   }
 
+cloud : Platform
+cloud =
+  { x = 100
+  , y = 400
+  , w = 50
+  , vx = 0
+  , vy = 0
+  }
+
 
 -- UPDATE
 
-update : (Float, Keys) -> Model -> Model
-update (dt, keys) mario =
+update : (Float, Keys, Bool, Bool) -> Model -> Model
+update (dt, keys, isRunning, isMoonWalking) mario =
   mario
-    |> gravity dt
+    |> gravity (dt, cloud)
     |> jump keys
-    |> walk keys
-    |> physics dt
+    |> fall keys
+    |> walk (isRunning, isMoonWalking, keys)
+    |> physics (dt, cloud)
 
 
 jump : Keys -> Model -> Model
 jump keys mario =
-  if keys.y > 0 && mario.vy == 0
+  if keys.y > 0 -- && mario.vy == 0
     then { mario | vy = 6.0 }
     else mario
 
+fall : Keys -> Model -> Model
+fall keys mario =
+  if keys.y < 0
+    then { mario | vy = -6.0 }
+    else mario
 
-gravity : Float -> Model -> Model
-gravity dt mario =
+gravity : (Float, Platform) -> Model -> Model
+gravity (dt, cloud) mario =
   { mario |
-      vy = if mario.y > 0 then mario.vy - dt/4 else 0
+      vy =
+        if mario.y > 0 then
+          mario.vy - dt/5 else 0
   }
 
 
-physics : Float -> Model -> Model
-physics dt mario =
+physics : (Float, Platform) -> Model -> Model
+physics (dt, cloud) mario =
   { mario |
       x = mario.x + dt * mario.vx,
-      y = max 0 (mario.y + dt * mario.vy)
+      y =
+        if (mario.y >= cloud.y && (mario.x >= cloud.x && mario.x <= cloud.x + cloud.w)) then
+          max cloud.y (mario.y + dt * mario.vy)
+        else max 0 (mario.y + dt * mario.vy)
   }
 
-
-walk : Keys -> Model -> Model
-walk keys mario =
+walk : (Bool, Bool, Keys) -> Model -> Model
+walk (isRunning, isMoonWalking, keys) mario =
   { mario |
-      vx = toFloat keys.x,
+      vx =
+        if isRunning then
+          if isMoonWalking then toFloat keys.x * -4
+          else toFloat keys.x * 4
+        else if isMoonWalking then toFloat keys.x * -1
+        else toFloat keys.x * 1,
       dir =
         if keys.x < 0 then Left
         else if keys.x > 0 then Right
         else mario.dir
+
   }
 
 
@@ -100,7 +133,7 @@ view (w',h') mario =
     marioImage =
       image 35 35 src
 
-    groundY = 62 - h/2
+    groundY = 32 - h/2
 
     position =
       (mario.x, mario.y + groundY)
@@ -111,6 +144,12 @@ view (w',h') mario =
       , rect w 50
           |> filled (rgb 74 167 43)
           |> move (0, 24 - h/2)
+      , rect w 50
+          |> filled (rgb 74 167 43)
+          |> move (0, 24 - h/2)
+      , rect 100 50
+          |> filled (rgb 255 255 255)
+          |> move (100, 400 - h/2)
       , marioImage
           |> toForm
           |> move position
@@ -124,9 +163,9 @@ main =
   Signal.map2 view Window.dimensions (Signal.foldp update mario input)
 
 
-input : Signal (Float, Keys)
+input : Signal (Float, Keys, Bool, Bool)
 input =
   let
     delta = Signal.map (\t -> t/20) (fps 30)
   in
-    Signal.sampleOn delta (Signal.map2 (,) delta Keyboard.arrows)
+    Signal.sampleOn delta (Signal.map4 (,,,) delta Keyboard.arrows Keyboard.shift Keyboard.space)
